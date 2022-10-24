a71cdd55de57a6e5dd63d065ea5df25a732d5f28
duvis07 <duvanarleybotero@gmail.com>
2022-10-24 18:36:10 -0500

    commit inicial

---
+++ src/main/java/com/iud/appsenado/AppSenadoApplication.java
@@ -0,0 +1,13 @@
+package com.iud.appsenado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSenadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSenadoApplication.class, args);
+	}
+
+}
